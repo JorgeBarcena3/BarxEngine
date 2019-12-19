cffc65524b829232b8cad09f6d8899e356ad30ba
Jorge Bárcena <j.barcenalumbreras@gmail.com>
2019-12-19 17:24:38 +0100

    BAudio Volume

--- Demo/code/source/main.cpp
+++ Demo/code/source/main.cpp
@@ -1,6 +1,24 @@
+// File: main.cpp
+// Author: Jorge Bárcena Lumbreras
+
+// © Copyright (C) 2019  Jorge Bárcena Lumbreras
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
+
 #include "BEngine.hpp"
 #include <iostream>
-
 
 int main() {
 
@@ -19,11 +37,12 @@ int main() {
     BAudio audio;
 
     Id id = audio.loadMusic("media\\testogg.ogg");
-    Id id2 = audio.loadMusic("media\\testmp3.mp3");
+    //Id id2 = audio.loadMusic("media\\testmp3.mp3");
     Id id3 = audio.loadSound("media\\testwaw.wav");
 
     audio.startMusic(id);
 
+	std::cout << id << endl << id3;
     std::getchar();
 
     return 0;
